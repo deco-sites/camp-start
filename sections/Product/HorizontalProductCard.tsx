a129c210ef871415847de2fa25b6202c1b30a992
Maiza <021maizadv@gmail.com>
2024-04-21 21:06:21 -0300

    feat: WIP

--- sections/Product/HorizontalProductCard.tsx
+++ sections/Product/HorizontalProductCard.tsx
@@ -1,32 +1,43 @@
+import type { Product } from "apps/commerce/types.ts";
 import { ImageWidget } from "apps/admin/widgets.ts";
 
 export interface Props {
-  myProduct: {
-    image: ImageWidget;
-    name: string;
-    price: number;
-    description: string;
-    textBtn: string;
-  };
+  /** @title Listagem de produtos */
+  products: Product[] | null;
 }
 
-export default function Section({ myProduct }: Props) {
+export default function Section({ products }: Props) {
+  if (!products || products.length === 0) {
+    return null;
+  }
+
   return (
-    <div class="flex flex-row sm:flex-col gap-4 items-center m-4">
-      <figure>
-        <img
-          width="200"
-          height="279"
-          src={myProduct.image}
-          alt={myProduct.name}
-        />
-      </figure>
-      <div class="flex flex-col gap-1 items-center">
-        <h3 class="text-xl">{myProduct.name}</h3>
-        <div class="text-sm">{myProduct.description}</div>
-        <div class="text-sm">Preço: ${myProduct.price}</div>
-        <button class="btn">{myProduct.textBtn}</button>
-      </div>
+    <div class="w-full container py-8 flex flex-col gap-6 lg:py-10">
+      {products.map((product, index) => (
+        <div>
+          {product.name}
+        </div>
+      ))}
     </div>
   );
+  //   return (
+  //     <div class="flex flex-row sm:flex-col gap-4 items-center m-4">
+  //       {
+  //         /* <figure>
+  //         <img
+  //           width="200"
+  //           height="279"
+  //           src={myProduct.image}
+  //           alt={myProduct.name}
+  //         />
+  //       </figure>
+  //       <div class="flex flex-col gap-1 items-center">
+  //         <h3 class="text-xl">{myProduct.name}</h3>
+  //         <div class="text-sm">{myProduct.description}</div>
+  //         <div class="text-sm">Preço: ${myProduct.price}</div>
+  //         <button class="btn">{myProduct.textBtn}</button>
+  //       </div> */
+  //       }
+  //     </div>
+  //   );
 }
